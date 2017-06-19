ac75cb52171231737d86436f47d2b663f5a925be
ianagez <ianagez88@gmail.com>
2017-06-19 18:20:25 +0300

    work on tests

---
+++ page_objects/DBActions.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ianagez on 19.06.17.
+ */
+module.exports={
+   // commands:[startUpParamsCommands],
+    elements:{
+        createDBBtn:'#create_db_btn',
+        dropDBBtn:'#drop_db_btn',
+        backupBtn:'#backup_db_btn',
+        restoreBtn:'#restore_db_btn',
+
+        createDBResultField:"#create_db_result",
+        dropDBResultField:'#drop_db_result',
+        backupDBResultField:'#backup_db_result',
+        restoreDBResultField:'#restore_db_result',
+
+        authAdminName:'#admin_name',
+        authAdminPas:'#admin_password',
+
+        authAdminDialog: '#adminAuthDialog',  //'div[id="adminAuthDialog"]'
+        backupDialog: '#backupDialog',
+        rewriteBackupDialog: '#rewriteBackupDialog',
+        restoreDialog: '#restoreDialog',
+        rewriteDBDialog: '#rewriteDBDialog'
+    }
+};
--- tests/pageObjectTests.js
+++ tests/pageObjectTests.js
@@ -16,83 +16,124 @@ module.exports={
             .assert.visible('@StartUpParamsBtn')
             .click('@StartUpParamsBtn');
     },
-    'Sysadmin Startup params Tests': function (browser) {
-        var paramsPage = browser.page.startUpParams();
-        paramsPage
-           .waitForElementVisible('@localConfigInfo',1000, true, function(){
-                browser.pause(2000, function(){
-                    paramsPage.assert.containsText('@localConfigInfo', "Configuration loaded.")
-                });
-            })
-        .click('@loadSettingsBtn', function(){
-                browser.pause(5000, function(){
-                    paramsPage.assert.containsText('@localConfigInfo', "Configuration reloaded.")
-                });
-            })
-            .waitForElementVisible('@dbHost', 2000)
-            .clearValue('@dbHost', function () {
-              //  paramsPage.assert.valueContains('@dbHost', '')
-                  paramsPage.setValue('@dbHost', '192.168.0.93_false', function () {
-                        var mainHeader=browser.page.sysadminHeader();
-                        paramsPage
-                            .click('@StoreAndReconnectBtn', function(){
-                                browser.pause(6000, function(){
-                                    paramsPage.assert.containsText('@localConfigInfo', "Configuration saved.")
-                                        .assert.containsText('@localConfigInfo', "Failed to connect to database!")
-                                });
-                            });
-                        mainHeader
-                            .waitForElementVisible('@dbConnectionState', 1000)
-                            .assert.containsText("@dbConnectionState","Failed to connect to database!");
-                    });
-            })
-            .resetDBConfig()
-            .waitForElementVisible('@dbName', 1000)
-            .clearValue('@dbName', function () {
-                //  paramsPage.assert.valueContains('@dbHost', '')
-                paramsPage.setValue('@dbName', 'GMSSample38xml_false', function () {
-                    var mainHeader=browser.page.sysadminHeader();
-                    paramsPage
-                        .click('@StoreAndReconnectBtn', function(){
-                            browser.pause(1000, function(){
-                                paramsPage.assert.containsText('@localConfigInfo', "Configuration sav")
-                                    .assert.containsText('@localConfigInfo', "Failed to connect to database!")
-                            });
-                        });
-                    mainHeader.waitForElementVisible('@dbName', 1000)
-                        .assert.containsText("@dbName","GMSSample38xml_false")
-                        .waitForElementVisible('@dbConnectionState', 1000)
-                        .assert.containsText("@dbConnectionState","Failed to connect to database!");
-                });
-            })
-            .resetDBConfig()
-            .waitForElementVisible('@dbUser', 1000)
-            .clearValue('@dbUser', function () {
-                paramsPage.assert.valueContains('@dbUser', '')
-                    .setValue('@dbUser', 'sa1', function () {
-                        var mainHeader=browser.page.sysadminHeader();
-                        paramsPage.assert.valueContains('@dbUser', 'sa1')
-                                  .click('@StoreAndReconnectBtn');
-                        mainHeader.waitForElementVisible('@user', 1000)
-                            .assert.containsText("@user","sa1")
-                            .waitForElementVisible('@dbConnectionState', 1000)
-                            .assert.containsText("@dbConnectionState","Failed to connect to database!");
-                    });
-            })
-            .resetDBConfig()
-            .waitForElementVisible('@dbPassword', 1000)
-            .clearValue('@dbPassword', function () {
-                paramsPage.assert.valueContains('@dbPassword', '')
-                    .setValue('@dbPassword', 'false', function () {
-                        var mainHeader=browser.page.sysadminHeader();
-                        paramsPage.assert.valueContains('@dbPassword', 'false')
-                            .click('@StoreAndReconnectBtn');
-                        mainHeader
-                            .waitForElementVisible('@dbConnectionState', 1000)
-                            .assert.containsText("@dbConnectionState","Failed to connect to database!");
-                    });
-            })
-        .resetDBConfig();
+    //'Sysadmin Startup params Tests': function (browser) {
+    //    var paramsPage = browser.page.startUpParams();
+    //    paramsPage
+    //       .waitForElementVisible('@localConfigInfo',1000, true, function(){
+    //            browser.pause(2000, function(){
+    //                paramsPage.assert.containsText('@localConfigInfo', "Configuration loaded.")
+    //            });
+    //        })
+    //    .click('@loadSettingsBtn', function(){
+    //            browser.pause(5000, function(){
+    //                paramsPage.assert.containsText('@localConfigInfo', "Configuration reloaded.")
+    //            });
+    //        })
+    //        .waitForElementVisible('@dbHost', 2000)
+    //        .clearValue('@dbHost', function () {
+    //          //  paramsPage.assert.valueContains('@dbHost', '')
+    //              paramsPage.setValue('@dbHost', '192.168.0.93_false', function () {
+    //                    var mainHeader=browser.page.sysadminHeader();
+    //                    paramsPage
+    //                        .click('@StoreAndReconnectBtn', function(){
+    //                            browser.pause(6000, function(){
+    //                                paramsPage.assert.containsText('@localConfigInfo', "Configuration saved.")
+    //                                    .assert.containsText('@localConfigInfo', "Failed to connect to database!")
+    //                            });
+    //                        });
+    //                    mainHeader
+    //                        .waitForElementVisible('@dbConnectionState', 1000)
+    //                        .assert.containsText("@dbConnectionState","Failed to connect to database!");
+    //                });
+    //        })
+    //        .resetDBConfig()
+    //        .waitForElementVisible('@dbName', 1000)
+    //        .clearValue('@dbName', function () {
+    //            //  paramsPage.assert.valueContains('@dbHost', '')
+    //            paramsPage.setValue('@dbName', 'GMSSample38xml_false', function () {
+    //                var mainHeader=browser.page.sysadminHeader();
+    //                paramsPage
+    //                    .click('@StoreAndReconnectBtn', function(){
+    //                        browser.pause(1000, function(){
+    //                            paramsPage.assert.containsText('@localConfigInfo', "Configuration sav")
+    //                                .assert.containsText('@localConfigInfo', "Failed to connect to database!")
+    //                        });
+    //                    });
+    //                mainHeader.waitForElementVisible('@dbName', 1000)
+    //                    .assert.containsText("@dbName","GMSSample38xml_false")
+    //                    .waitForElementVisible('@dbConnectionState', 1000)
+    //                    .assert.containsText("@dbConnectionState","Failed to connect to database!");
+    //            });
+    //        })
+    //        .resetDBConfig()
+    //        .waitForElementVisible('@dbUser', 1000)
+    //        .clearValue('@dbUser', function () {
+    //            paramsPage.assert.valueContains('@dbUser', '')
+    //                .setValue('@dbUser', 'sa1', function () {
+    //                    var mainHeader=browser.page.sysadminHeader();
+    //                    paramsPage.assert.valueContains('@dbUser', 'sa1')
+    //                              .click('@StoreAndReconnectBtn');
+    //                    mainHeader.waitForElementVisible('@user', 1000)
+    //                        .assert.containsText("@user","sa1")
+    //                        .waitForElementVisible('@dbConnectionState', 1000)
+    //                        .assert.containsText("@dbConnectionState","Failed to connect to database!");
+    //                });
+    //        })
+    //        .resetDBConfig()
+    //        .waitForElementVisible('@dbPassword', 1000)
+    //        .clearValue('@dbPassword', function () {
+    //            paramsPage.assert.valueContains('@dbPassword', '')
+    //                .setValue('@dbPassword', 'false', function () {
+    //                    var mainHeader=browser.page.sysadminHeader();
+    //                    paramsPage.assert.valueContains('@dbPassword', 'false')
+    //                        .click('@StoreAndReconnectBtn');
+    //                    mainHeader
+    //                        .waitForElementVisible('@dbConnectionState', 1000)
+    //                        .assert.containsText("@dbConnectionState","Failed to connect to database!");
+    //                });
+    //        })
+    //    .resetDBConfig();
+    //
+    //},
+    'DB Actions Tests': function (browser) {
+        var DBActions = browser.page.DBActions();
+        DBActions
+            .waitForElementVisible('@createDBBtn', 1000);
+            browser.pause(2000);
+        //.assert.visible('@createDBBtn')
+        DBActions
+            .click('@createDBBtn')
+            .waitForElementVisible('@authAdminDialog', 6000);
+
         browser.end();
+
+        //browser.execute(function () {
+        //    browser.querySelector('#create_db_btn').click()
+        //});
+        //DBActions
+
+           //
+           //     DBActions.elementIdClick('@createDBBtn', function (){
+           //         DBActions.waitForElementVisible('@authAdminDialog', 6000,false, function(){
+           //             browser.end();
+           //         });
+           //     });
+           // });
+           //// .click('@createDBBtn', function () {
+           //     var DBActions = browser.page.DBActions();
+           //     DBActions.waitForElementVisible('@authAdminDialog', 6000);
+                //.assert.value('@authAdminName', 'root')
+                //.assert.value('@authAdminPas', '');
+
+                //this.waitForElementVisible('@authAdminName', 1000);
+              //  this.assert.value('@authAdminName', 'root');
+              //  browser.end();
+          //  });
+        //.waitForElementVisible('@authAdminDialog', 2000)
+        //.assert.value('@authAdminName', 'root')
+        //.assert.value('@authAdminPas', '');
+
+
     }
+
 };
