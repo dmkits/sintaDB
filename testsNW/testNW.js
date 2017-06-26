657ffb2e9a1dc98091409d11bdba1c295efe83c7
ianagez <ianagez88@gmail.com>
2017-06-26 18:40:48 +0300

    test input

--- testsNW/testNW.js
+++ testsNW/testNW.js
@@ -11,10 +11,14 @@ module.exports= {
      * params = { title, content }
      */
     findDojoStdDialog: function(params){
-        this.browser.pause(5000);
-        var dlg= this.browser.element('partial link text', 'PORT', function(res) {
-            console.log("\n \n RES=",res)
-        });
+       // this.browser.pause(5000);
+       // var dlg= this.browser.element('link text', 'PORT', function(res) {
+       //     console.log("\n \n RES=",res)
+       // });
+
+        this.browser
+            .pause(5000)
+            .useXpath().assert.elementPresent("//*[contains(text(), 'params')]");  //
 
         //console.log("findDojoStdDialog=",dlg);
 
@@ -24,16 +28,75 @@ module.exports= {
 
         return this;
     },
-    assertEnabled: function(){
+    /*
+     * params = { parentClass, tag, class, text }
+     */
+    assertElementVisible: function(params) {
+        var pathToElement=getXpath(params);
 
+        this.browser
+            .useXpath()
+            .waitForElementVisible(pathToElement,1000)
+            .assert.visible(pathToElement);
+            //.expect.element(pathToElement).to.be.visible;
         return this;
     },
-    assertVisible: function(){
+    assertElementContainsValue: function(params) {
+       // var pathToElement=getXpath(params);
 
+        this.browser
+            .useXpath()
+       //.elements('xpath', 'input', function(res) {
+       //     console.log("assertElementContainsValue res=",res)
+       // });
+           // .waitForElementVisible("//*input[@value='localhost']",1000)
+            .waitForElementVisible("//input[contains(value(), 'localhost')]",1000);
+           // .assert.visible("//*input[@value='localhost']");
+          // .expect.element(pathToElement).to.have.attribute('value').equals(params.value);
         return this;
     },
-    click: function(){
 
+
+    clickDojoButton: function(params){
+        var pathToElement=getXpath(params);
+
+        this.browser
+            .useXpath()
+            .waitForElementVisible(pathToElement,1000)
+           // .expect.element(pathToElement).to.be.visible
+            .assert.visible(pathToElement)
+            .click(pathToElement);
+        return this;
+    },
+    checkDojoToggleButtonSelected: function(params){
+        var pathToElement=getXpath(params)+"/..";
+       // var pathToElement=getParentNode(params);
+
+        this.browser
+            .pause(1000)
+            .useXpath()
+            .expect.element(pathToElement).to.have.attribute('aria-pressed').equals('true');
         return this;
     }
 };
+function getXpath(params) {
+    var xpathStr = "";
+    if (params.parentClass)xpathStr = "//*[@class=\"" + xpathStr + params.parentClass + "\"]";
+    if (params.tag) {
+        xpathStr = xpathStr + "//" + params.tag
+    } else {
+        xpathStr = xpathStr + "//*"
+    }
+    if (params.class) {
+        xpathStr = xpathStr + "[@class=\"" + params.class + "\"";
+        if (params.text) {
+            xpathStr = xpathStr + "  and contains(text(), '" + params.text + "')] "
+        }else{
+            xpathStr=xpathStr+"] "
+        }
+    }
+    if(params.text &&!params.class){
+        xpathStr=xpathStr+"[contains(text(), '"+params.text+"')] "
+    }
+    return xpathStr;
+}
--- testsNW/tests/testSysadmin.js
+++ testsNW/tests/testSysadmin.js
@@ -2,11 +2,99 @@ var tnw= require('../testNW');
 module.exports= {
     'Sysadmin Header Tests': function (browser) {
 
-
-
         tnw.start(browser, {url:'http://localhost:8080/sysadmin'})
-            .findDojoStdDialog({title:'Title',content:'lsdfmklsdmfklmds'}).assertVisible()
-            .findDojoButtonWithText('Login').assertEnabled().assertVisible().click()
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'img'})
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'b',text:'MODE'})
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'b',text:'PORT'})
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'b',text:'DB NAME'})
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'b',text:'USER'})
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'b',text:'DB CONNECTION STATE'})
+
+            .assertElementVisible({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'span',text:'Startup params'})
+            .clickDojoButton({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'span',text:'Startup params'})
+            .checkDojoToggleButtonSelected({parentClass:'dijitBorderContainer dijitContainer sysadmin_TopContent dijitBorderContainer-child dijitBorderContainer-dijitBorderContainer dijitBorderContainerPane dijitAlignTop dijitLayoutContainer',
+                tag:'span',text:'Startup params'})
+
+            .assertElementVisible({class:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'system startup parameters'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'Configuration loaded.'})
+
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"label",
+                text:'db.host'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'Configuration loaded.'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"label",
+                text:'db.name'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"label",
+                text:'db.user'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"label",
+                text:'db.password'})
+
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"span",
+                text:'Load settings'})
+            .clickDojoButton({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:'span',text:'Load settings'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'Configuration reloaded.'})
+
+            .assertElementContainsValue()
+
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"span",
+                text:'Store settings & reconnect to database'})
+            .clickDojoButton({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:'span',text:'Store settings & reconnect to database'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'Configuration saved.'})
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"b",
+                text:'Reconnected to database.'})
+
+            .assertElementVisible({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:"span",
+                text:'Create DB'})
+            .clickDojoButton({parentClass:'dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild',
+                tag:'span',text:'Create DB'})
+
+            .assertElementVisible({
+                tag:"div",
+                class:'dijitDialog dijitDialogFocused dijitFocused'})
+
+        ;
+
+
+
+
+
+
+        //dijitContentPane dijitStackContainer-child dijitStackContainer-dijitContentPane dijitContentPaneSingleChild
+
+
+
+
+
+            //.assertVisible();
+            //.findDojoButtonWithText('Login').assertVisible().click();
 
     //    browser.element('link Text selector', 'PORT', function(res) {
     //        console.log(res)
